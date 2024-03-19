bdd3b2a61c336019dd88d6c7e2fa7dd9408d1590
foysal-mahmud-hasan <1730150@iub.edu.bd>
2024-03-19 11:37:43 +0600

    update check 01

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,24 +1,51 @@
 import { Title } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react';
+import { Modal, Group, Button } from '@mantine/core';
+
 const LandingPage = () => {
+
+    const [slowTransitionOpened, setSlowTransitionOpened] = useState(false);
 
     return (
 
         <main  >
-            <Title c="red">Home</Title>
-            {/* Your landing page content goes here */}
-            <section>
-                {/* Hero section */}
-                <h1>Hello World! , Welcome to my Matine app!</h1>
-                <p>This is the landing page . please press the buttons on the navbar to navigate</p>
-            </section>
-            <section>
-                {/* Additional sections as needed */}
-                <h2>List of functionalities</h2>
-                <p>1. Used Mantine AppShell <br />
-                    2. Used React router dom to navigate <br />
-                    3. Used validation on form .
-                </p>
-            </section>
+            <>
+                <Title c="red">Home</Title>
+                {/* Your landing page content goes here */}
+                <section>
+                    {/* Hero section */}
+                    <h1>Hello World! , Welcome to my Matine app!</h1>
+                    <p>This is the landing page . please press the buttons on the navbar to navigate</p>
+                </section>
+                <section>
+                    {/* Additional sections as needed */}
+                    <h2>List of functionalities</h2>
+                    <p>1. Used Mantine AppShell <br />
+                        2. Used React router dom to navigate <br />
+                        3. Used validation on form .
+                    </p>
+                </section>
+
+
+                <Modal
+                    opened={slowTransitionOpened}
+                    onClose={() => setSlowTransitionOpened(false)}
+                    title="Please consider this"
+                    transitionProps={{ transition: 'rotate-left' }}
+                >
+                    rotate-left transition
+                </Modal>
+
+
+
+                <Group justify="center">
+                    <Button onClick={() => setSlowTransitionOpened(true)} variant="default">
+                        Rotate left transition
+                    </Button>
+
+                </Group>
+            </>
 
         </main>
     )
--- src/components/table/tablePagination.jsx
+++ src/components/table/tablePagination.jsx
@@ -37,30 +37,7 @@ export default function Pagination() {
         setOpened(false);
     };
 
-    const showModal = ({ company, action }) => {
-        const ModalContent = ({ company, action }) => (
-            <div>
-                <p>
-                    {action === 'edit'
-                        ? 'Here\'s where you could put an edit form...'
-                        : 'Here\'s where you could ask for confirmation before deleting...'}
-                </p>
-                <div>
-                    <div>ID</div>
-                    <div>{company.id}</div>
-                    <div>Name</div>
-                    <div>{company.name}</div>
-                </div>
-                <button onClick={() => closeModal(action)}>Close</button>
-            </div>
-        );
 
-        openModal({
-            modalId: action,
-            title: action === 'edit' ? 'Editing company information' : 'Deleting company',
-            children: <ModalContent company={company} action={action} />,
-        });
-    };
 
 
     useEffect(() => {
@@ -76,100 +53,117 @@ export default function Pagination() {
         setRecords(companies.slice(from, to));
     }, [page, pageSize]);
 
+
+    const [slowTransitionOpened, setSlowTransitionOpened] = useState(false);
+
+
     return (
-        <DataTable
-            withTableBorder
-            borderRadius="sm"
-            shadow="sm"
-            withColumnBorders
-            striped
-            highlightOnHover
-            horizontalSpacing="xs"
-            verticalSpacing="xs"
-            fz="sm"
-            verticalAlign="center"
-            //  override default text color for light and dark themes
-            c={{ dark: '#dbc7a0', light: '#55350d' }}
-            //  override default background color for light and dark themes
-            backgroundColor={{ dark: '#232b25', light: '#f0f7f1' }}
-            borderColor="#40c057" //  override default border color
-            rowBorderColor="#fab005" //  override default row border color
-            paginationActiveBackgroundColor="#40c057" //  override default pagination active background color
-            records={records}
-            columns={[
-                { accessor: 'name', },
-                { accessor: 'city', },
-                { accessor: 'state', },
-                {
-                    accessor: 'actions',
-                    title: <Box mr={6}>Row actions</Box>,
-                    textAlign: 'right',
-                    render: (company) => (
-                        <Group gap={4} justify="right" wrap="nowrap">
-                            <ActionIcon
-                                size="sm"
-                                variant="subtle"
-                                color="green"
-                                onClick={() => showModal({ company, action: 'view' })}
-                            >
-                                <FontAwesomeIcon icon={faEye} />
-                            </ActionIcon>
-                            <ActionIcon
-                                size="sm"
-                                variant="subtle"
-                                color="blue"
-                                onClick={() => showModal({ company, action: 'edit' })}
-                            >
-                                <FontAwesomeIcon icon={faEdit} />
-                            </ActionIcon>
-                            <ActionIcon
-                                size="sm"
-                                variant="subtle"
-                                color="red"
-                                onClick={() => showModal({ company, action: 'delete' })}
-                            >
-                                <FontAwesomeIcon icon={faTrash} />
-                            </ActionIcon>
-                        </Group>
-                    ),
-                },
-            ]}
-            totalRecords={companies.length}
-            recordsPerPage={pageSize}
-            page={page}
-            onPageChange={(p) => setPage(p)}
-            recordsPerPageOptions={PAGE_SIZES}
-            onRecordsPerPageChange={setPageSize}
-            //  uncomment the next line to use a custom pagination size
-            // paginationSize="md"
-            //  uncomment the next line to use a custom loading text
-            // loadingText="Loading..."
-            //  uncomment the next line to display a custom text when no records were found
-            // noRecordsText="No records found"
-            //  customize pagination positioning and appearance
+        <>
 
+            <Modal
+                opened={slowTransitionOpened}
+                onClose={() => setSlowTransitionOpened(false)}
+                title="Please consider this"
+                transitionProps={{ transition: 'rotate-left' }}
+            >
+                rotate-left transition
+            </Modal>
+
+            <DataTable
+                withTableBorder
+                borderRadius="sm"
+                shadow="sm"
+                withColumnBorders
+                striped
+                highlightOnHover
+                horizontalSpacing="xs"
+                verticalSpacing="xs"
+                fz="sm"
+                verticalAlign="center"
+                //  override default text color for light and dark themes
+                c={{ dark: '#dbc7a0', light: '#55350d' }}
+                //  override default background color for light and dark themes
+                backgroundColor={{ dark: '#232b25', light: '#f0f7f1' }}
+                borderColor="#40c057" //  override default border color
+                rowBorderColor="#fab005" //  override default row border color
+                paginationActiveBackgroundColor="#40c057" //  override default pagination active background color
+                records={records}
+                columns={[
+                    { accessor: 'name', },
+                    { accessor: 'city', },
+                    { accessor: 'state', },
+                    {
+                        accessor: 'actions',
+                        title: <Box mr={6}>Row actions</Box>,
+                        textAlign: 'right',
+                        render: (company) => (
+                            <Group gap={4} justify="right" wrap="nowrap">
+                                <ActionIcon
+                                    size="sm"
+                                    variant="subtle"
+                                    color="green"
+                                    onClick={() => setSlowTransitionOpened(true)}
+                                >
+                                    <FontAwesomeIcon icon={faEye} />
+                                </ActionIcon>
+                                <ActionIcon
+                                    size="sm"
+                                    variant="subtle"
+                                    color="blue"
+                                // onClick={() => showModal({ company, action: 'edit' })}
+                                >
+                                    <FontAwesomeIcon icon={faEdit} />
+                                </ActionIcon>
+                                <ActionIcon
+                                    size="sm"
+                                    variant="subtle"
+                                    color="red"
+                                // onClick={() => showModal({ company, action: 'delete' })}
+                                >
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </ActionIcon>
+                            </Group>
+                        ),
+                    },
+                ]}
+                totalRecords={companies.length}
+                recordsPerPage={pageSize}
+                page={page}
+                onPageChange={(p) => setPage(p)}
+                recordsPerPageOptions={PAGE_SIZES}
+                onRecordsPerPageChange={setPageSize}
+                //  uncomment the next line to use a custom pagination size
+                // paginationSize="md"
+                //  uncomment the next line to use a custom loading text
+                // loadingText="Loading..."
+                //  uncomment the next line to display a custom text when no records were found
+                // noRecordsText="No records found"
+                //  customize pagination positioning and appearance
+
+                /* pagination={{
+                  position: 'bottom', // Place it at the bottom
+                  style: customPaginationStyles, // Apply custom styles directly
+                }}
+           */
+                pagination={{
+                    position: 'bottom', // Place it at the bottom
+                    className: styles.pagination, // Apply custom styles
+                }}
             /* pagination={{
-              position: 'bottom', // Place it at the bottom
-              style: customPaginationStyles, // Apply custom styles directly
-            }}
-       */
-            pagination={{
-                position: 'bottom', // Place it at the bottom
-                className: styles.pagination, // Apply custom styles
-            }}
-        /* pagination={{
-          position: 'bottom-left !important', // Move pagination to bottom-left corner
-          // Additional customization options for pagination styling (optional)
-          // style: {
-          //   // Modify styles as needed to achieve your desired look
-          // }
-        }} */
-        //  uncomment the next lines to use custom pagination text
-        // paginationText={({ from, to, totalRecords }) => `Records ${from} - ${to} of ${totalRecords}`}
-        //  uncomment the next lines to use custom pagination colors
-        // paginationActiveBackgroundColor="green"
-        // paginationActiveTextColor="#e6e348"
+              position: 'bottom-left !important', // Move pagination to bottom-left corner
+              // Additional customization options for pagination styling (optional)
+              // style: {
+              //   // Modify styles as needed to achieve your desired look
+              // }
+            }} */
+            //  uncomment the next lines to use custom pagination text
+            // paginationText={({ from, to, totalRecords }) => `Records ${from} - ${to} of ${totalRecords}`}
+            //  uncomment the next lines to use custom pagination colors
+            // paginationActiveBackgroundColor="green"
+            // paginationActiveTextColor="#e6e348"
 
-        />
+            />
+        </>
+
     );
 }
